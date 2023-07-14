665fdee8bd125461161ccd34891ac71be384e551
Stefan Vitoria <steph.74lw@gmail.com>
2023-07-13 23:10:05 -0100

    feat: created ListingReservation and Calendar component to suport date range selection with the help of react-date-range

--- src/app/components/listings/ListingReservation.tsx
+++ src/app/components/listings/ListingReservation.tsx
@@ -1,9 +1,37 @@
+'use client';
+
+import { Range } from "react-date-range";
+import Calendar from "../inputs/Calendar";
+
+interface ListingReservationProps {
+    price: number;
+    totalPrice: number;
+    onChangeDate: (value: Range) => void;
+    dateRange: Range;
+    onSubmit: () => void;
+    disabled?: boolean;
+    disabledDates: Date[];
+}
 
 
-const ListingReservation = () => {
-  return (
-    <div>ListingReservation</div>
-  )
+const ListingReservation: React.FC<ListingReservationProps> = ({ price, totalPrice, onChangeDate, dateRange, onSubmit, disabled, disabledDates }) => {
+
+    return (
+
+        <article className='bg-white rounded-xl border-[1px] border-neutral-200 overflow-hidden' >
+            <article className='flex flex-row items-center gap-1 p-4' >
+                <span className='text-2xl font-semibold' >$ {price}</span>
+                <span className='text-neutral-600 font-light' >/night</span>
+            </article>
+            <hr />
+            <Calendar
+                value={dateRange}
+                disabledDates={disabledDates}
+                onChange={(value) => onChangeDate(value.selection)}
+            />
+        </article>
+
+    )
 }
 
 export default ListingReservation;
